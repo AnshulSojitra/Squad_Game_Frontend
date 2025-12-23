8434e00c26aac3144c76a44710c0d6760858e892
Anshul <anshul.patel321@gmail.com>
2025-12-23 15:42:35 +0530

    coomit before merge

--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -32,9 +32,7 @@ export default function AdminLogin() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
-        <h2 className="text-2xl font-semibold text-center mb-6">
-          Login
-        </h2>
+        <h2 className="text-2xl font-semibold text-center mb-6">Login</h2>
 
         <form onSubmit={handleLogin}>
           <Input
